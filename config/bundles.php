7900debf0e92e5c67928626e56619c69ff7cd0d1
Michał Hepner <michal.hepner@ffwagency.com>
2018-08-29 12:24:34 +0200

    Remove unused packages

--- config/bundles.php
+++ config/bundles.php
@@ -2,8 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
